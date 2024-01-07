93eba2cadf1ddbcc810841b546cfa6f3dbc5d669
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2024-01-07 17:13:27 -0500

    Initial Commit

---
+++ Today/TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayApp.swift
+//  Today
+//
+//  Created by Gerard Gomez on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
